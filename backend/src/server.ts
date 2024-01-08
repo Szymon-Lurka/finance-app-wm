05ba83ba51466ecd24aa9e482b87b17a3e1de2fe
Szymek <szymon.lurka@gmail.com>
2024-01-08 17:24:01 +0100

    test gh actions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.json({status: 'success', data: [1, 2, 3, 4, 5]});
+    res.json({status: 'success', data: [1, 2, 3, 4, 5, 'tessssst']});
 })
 
 app.listen(3001, () => {
